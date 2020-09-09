b254b152a2c42384ea7608d4c10ad4ab90c559d5
SaqibAleem <saqib.py@gmail.com>
2020-09-09 14:09:08 +0500

    added cdn

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,8 +1,4 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
-  $('#dataTable_paginate').addClass('float-right');
-  $('#dataTable_filter').addClass('float-right');
-});
+
 
 
